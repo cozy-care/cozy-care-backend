5388eaaf4e1af4ceb1c0b5a1bfee74699a2ca3dd
Belle <bell71457145@gmail.com>
2024-10-09 13:04:27 +0700

    uncomment autController and authMiddleware

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -1,15 +1,15 @@
-// const jwt = require('jsonwebtoken');
+const jwt = require('jsonwebtoken');
 
-// function authenticateToken(req, res, next) {
-//   const token = req.cookies.token;
+function authenticateToken(req, res, next) {
+  const token = req.cookies.token;
 
-//   if (!token) return res.sendStatus(401);
+  if (!token) return res.sendStatus(401);
 
-//   jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
-//     if (err) return res.sendStatus(403);
-//     req.user = user;
-//     next();
-//   });
-// }
+  jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
+    if (err) return res.sendStatus(403);
+    req.user = user;
+    next();
+  });
+}
 
-// module.exports = { authenticateToken };
+module.exports = { authenticateToken };
