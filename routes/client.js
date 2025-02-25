73d54c4eebee736516696a065c55cddf8153a213
Raweeroj Thokaeo <ajdvdsf.aj@gmail.com>
2025-02-25 22:08:44 +0700

    create client API

--- routes/client.js
+++ routes/client.js
@@ -1,8 +1,12 @@
 const express = require('express');
-const { createSubClient } = require('../controllers/clientController');
+const { createSubClient, getSubClient, delSubClient, getSubClientDetail, updateSubClientDetail } = require('../controllers/clientController');
 
 const router = express.Router();
 
 router.post('/create-sub-client', createSubClient);
+router.post('/get-sub-client', getSubClient);
+router.post('/delete-sub-client', delSubClient);
+router.post('/get-sub-client-details', getSubClientDetail);
+router.put('/update-sub-client-details', updateSubClientDetail)
 
 module.exports = router;
